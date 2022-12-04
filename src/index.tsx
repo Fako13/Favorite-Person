8fb044359880c517452a2a6cc8b43c034176dd15
Sergey Neskin <neskin.x@mail.ru>
2022-12-04 20:48:49 +0700

    added fonts and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FontStyle from './assets/styles/fonts'
+import GlobalStyle from './assets/styles/global'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<FontStyle />
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
